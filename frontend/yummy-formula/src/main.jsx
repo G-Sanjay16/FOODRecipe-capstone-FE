6070c72f35b1779b4fa176ae5ecf9da967d7abd4
G-Sanjay16 <sanjay.ganesan0816@gmail.com>
2024-09-02 09:58:58 +0530

    CHANGED

--- frontend/yummy-formula/src/main.jsx
+++ frontend/yummy-formula/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import store from "./Redux/Store.jsx";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>,
+  </Provider>
 );
